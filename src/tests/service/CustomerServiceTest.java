72c2a2fdca7ab0534315e69af6753262a9de9706
denis3119 <denis3119@gmail.com>
2016-04-13 23:06:54 +0300

    revert tests

--- src/tests/service/CustomerServiceTest.java
+++ src/tests/service/CustomerServiceTest.java
@@ -1,61 +1,35 @@
-//package service;
-//
-//import com.e_commerce.config.Application;
-//import com.e_commerce.data.model.user.Customer;
-//import com.e_commerce.data.repository.UserRepository;
-//import com.e_commerce.service.UserService;
-//import org.junit.After;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.SpringApplicationConfiguration;
-//import org.springframework.test.context.ContextConfiguration;
-//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-//import org.springframework.test.context.web.WebAppConfiguration;
-//
-//import static com.e_commerce.data.model.user.Role.ROLE_USER;
-//import static org.junit.Assert.*;
-//
-///**
-// * Created by admin on 22.01.2016.
-// */
-//@RunWith(SpringJUnit4ClassRunner.class)
-//@ContextConfiguration(classes = Application.class)
-//@SpringApplicationConfiguration(classes = Application.class)
-//@WebAppConfiguration
-//public class CustomerServiceTest {
-//    @Autowired
-//    UserService userService;
-//    @Autowired
-//    UserRepository userRepository;
-//    private String testEmail = "test@test.com";
-//
-//    @Test
-//    public void Register() {
-//        Customer customer = createUser();
-//        assertNotNull(customer);
-//        assertNotNull(customer.getUserRoles());
-//        assertTrue(customer.getUserRoles().iterator().next().getRole().equals(ROLE_USER.toString()));
-//    }
-//
-//    @Test
-//    public void Delete() {
-//        Customer user = createUser();
-//        userRepository.delete(user);
-//        assertNull(userService.getByEmail(testEmail));
-//    }
-//
-//
-//    private Customer createUser() {
-//        Customer customer = new Customer("password", testEmail);
-//        return userService.register(customer);
-//    }
-//
-//    @After
-//    public void deleteUser() {
-//        Customer customer = userService.getByEmail(testEmail);
-//        if (customer != null) {
-//            userRepository.delete(customer);
-//        }
-//    }
-//}
+package service;
+
+import com.e_commerce.data.model.user.Customer;
+import com.e_commerce.data.repository.UserRepository;
+import com.e_commerce.service.UserService;
+import com.e_commerce.service.impl.UserServiceImpl;
+import org.junit.After;
+import org.junit.Test;
+
+import static com.e_commerce.data.model.user.Role.ROLE_USER;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+
+/**
+ * Created by admin on 22.01.2016.
+ */
+
+public class CustomerServiceTest {
+    private UserService userService = mock(UserServiceImpl.class);
+    private String testEmail = "test@test.com";
+    private Customer customer = createUser();
+
+    @Test
+    public void Register() {
+        assertNotNull(customer);
+        assertNotNull(customer.getUserRoles());
+    }
+
+
+    private Customer createUser() {
+        Customer customer = new Customer("password", testEmail);
+        return customer;
+    }
+
+}
--- src/tests/service/ProductServiceTest.java
+++ src/tests/service/ProductServiceTest.java
@@ -1,88 +1,29 @@
-//package service;
-//
-//import com.e_commerce.config.Application;
-//import com.e_commerce.data.model.Product.Category;
-//import com.e_commerce.data.model.Product.Product;
-//import com.e_commerce.data.repository.CategoryRepository;
-//import com.e_commerce.data.repository.ProductRepository;
-//import com.e_commerce.service.CategoryService;
-//import com.e_commerce.service.ProductService;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.SpringApplicationConfiguration;
-//import org.springframework.test.context.ContextConfiguration;
-//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-//import org.springframework.test.context.web.WebAppConfiguration;
-//
-//import java.util.HashSet;
-//
-//import static org.junit.Assert.*;
-//
-///**
-// * Created by admin on 26.01.2016.
-// */
-//@RunWith(SpringJUnit4ClassRunner.class)
-//@ContextConfiguration(classes = Application.class)
-//@SpringApplicationConfiguration(classes = Application.class)
-//@WebAppConfiguration
-//public class ProductServiceTest {
-//    @Autowired
-//    ProductService productService;
-//    @Autowired
-//    ProductRepository productRepository;
-//    @Autowired
-//    CategoryRepository categoryRepository;
-//    @Autowired
-//    CategoryService categoryService;
-//
-//    @Test
-//    public void createProduct() {
-//        Product product = create();
-//        assertNotNull(product);
-//        remove(product);
-//    }
-//
-//    @Test
-//    public void expiredProduct() {
-//        Product product = create();
-//        product = productService.expired(product);
-//        assertTrue(product.isExpired());
-//        remove(product);
-//    }
-//
-//    @Test
-//    public void joinCategory() {
-//        Product product = create();
-//        Category category = new Category();
-//        category.setName("category");
-//        category = categoryService.create(category);
-//        product = productService.joinCategory(product, category);
-//        assertFalse(product.getCategories().isEmpty());
-//        product.setCategories(new HashSet<>());
-//        product = productRepository.saveAndFlush(product);
-//        categoryRepository.delete(category);
-//        remove(product);
-//    }
-//
-//    @Test
-//    public void removeCategory(){
-//        Product product = create();
-//        Category category = new Category();
-//        category.setName("category");
-//        category = categoryService.create(category);
-//        product = productService.joinCategory(product, category);
-//        productService.removeCategory(product,category);
-//    }
-//
-//    private Product create() {
-//        Product product = new Product();
-//        return productService.create(product);
-//    }
-//
-//    private void remove(Product product) {
-//        if (productRepository.getOne(product.getId()) != null) {
-//            productRepository.delete(product);
-//        }
-//    }
-//}
+package service;
+
+import com.e_commerce.data.model.Product.Product;
+import com.e_commerce.service.CategoryService;
+import com.e_commerce.service.ProductService;
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * Created by admin on 26.01.2016.
+ */
+public class ProductServiceTest {
+    private ProductService productService;
+    private CategoryService categoryService;
+
+    @Test
+    public void createProduct() {
+        Product product = create();
+        assertNotNull(product);
+    }
+
+
+    private Product create() {
+        Product product = new Product();
+        return product;
+    }
+
+}
